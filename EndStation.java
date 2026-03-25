5cf3078d1d6da3aacd7065b60b0eab743ddb6474
alanam18 <alanam1@gwmail.gwu.edu>
2026-03-25 00:21:20 -0400

    Update EndStation to include makeEnd and toString

--- EndStation.java
+++ EndStation.java
@@ -1,1 +1,19 @@
+public class EndStation extends Station {
+ 
+    public EndStation(String line, String name) {
+        super(line, name);
+    }
+ 
+    public void makeEnd() {
+      if (next != null) {
+        prev = next;
+      } 
+      else {
+        next = prev;
+      }
+    }
 
+    public String toString() {
+      return super.toString().replace("STATION", "ENDSTATION");
+    }
+}
