307bd8b79e89985d3434b996bc6d9d55a7b4cd57
alanam18 <alanam1@gwmail.gwu.edu>
2026-03-25 00:03:12 -0400

    Added addNext, addPrev, connect methods and equals/toString to Station"

--- Station.java
+++ Station.java
@@ -14,3 +14,39 @@ public class Station {
         this.prev = null;
         this.next = null;
     }
+  public void addNext(Station s) {
+        this.next = s;
+        s.prev = this;
+    }
+ 
+    public void addPrev(Station s) {
+        this.prev = s;
+        s.next = this;
+    }
+ 
+    public void connect(Station s) {
+        this.next = s;
+        s.prev = this;
+    }
+ 
+    public boolean isAvailable() {
+        return inService;
+    }
+ 
+    public void switchAvailable() {
+        inService = !inService;
+    }
+ 
+    public boolean equals(Object o) {
+        if (!(o instanceof Station)) return false;
+        Station other = (Station) o;
+        return this.line.equals(other.line) && this.name.equals(other.name);
+    }
+ 
+    public String toString() {
+        String prevName = (prev == null) ? "none" : prev.name;
+        String nextName = (next == null) ? "none" : next.name;
+        return "STATION " + name + ": " + line + " line, in service: " + inService +
+               ", previous station: " + prevName + ", next station: " + nextName;
+    }
+ 
