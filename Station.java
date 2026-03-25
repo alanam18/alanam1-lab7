a4cffd65981102985219f99844b9508edf272b82
alanam18 <alanam1@gwmail.gwu.edu>
2026-03-24 23:34:00 -0400

    Added Station class with line, name, inService fields and constructor

--- Station.java
+++ Station.java
@@ -1,1 +1,16 @@
-
+import java.util.ArrayList;
+ 
+public class Station {
+    protected String line;
+    protected String name;
+    protected boolean inService;
+    protected Station prev;
+    protected Station next;
+ 
+    public Station(String line, String name) {
+        this.line = line;
+        this.name = name;
+        this.inService = true;
+        this.prev = null;
+        this.next = null;
+    }
